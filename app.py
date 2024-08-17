10eb9ca9880850d253aff2e06fb20ae274c87698
Kalpesh-Tarsariya <kalpesh.tarsariya819@gmail.com>
2024-08-17 17:17:10 +0530

    Data Ingestion

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-def addition(a,b):
-    return a+b
+from src.MLProject.logger import logging
+from src.MLProject.exception import CustomException
+import sys
+from src.MLProject.components.data_ingestion import DataIngestion, DataIngestionConfig
 
-def substraction(a,b):
-    return a-b
 
-def division(a,b):
-    a/b
+
+if __name__=="__main__":
+    logging.info("The execution has started.")
+
+    try:
+        #data_ingestion_config = DataIngestionConfig()
+        data_ingestion = DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+    except Exception as e:
+        logging.info("Custome Exception")
+        raise CustomException(e,sys)
+
